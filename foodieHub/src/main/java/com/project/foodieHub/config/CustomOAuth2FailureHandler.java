ad3b6e137e25d930eb501cf51055005e69ece6ef
SGraceGrace <143794400+SGraceGrace@users.noreply.github.com>
2025-07-07 20:53:44 +0530

    101 -> CustomOAuth2FailureHandler impl

--- foodieHub/src/main/java/com/project/foodieHub/config/CustomOAuth2FailureHandler.java
+++ foodieHub/src/main/java/com/project/foodieHub/config/CustomOAuth2FailureHandler.java
@@ -6,11 +6,13 @@ import jakarta.servlet.http.HttpServletResponse;
 import java.io.IOException;
 import org.springframework.security.core.AuthenticationException;
 import org.springframework.security.web.authentication.AuthenticationFailureHandler;
+
 public class CustomOAuth2FailureHandler implements AuthenticationFailureHandler {
 
   @Override
   public void onAuthenticationFailure(HttpServletRequest request, HttpServletResponse response,
       AuthenticationException exception) throws IOException, ServletException {
-
+    response.setStatus(HttpServletResponse.SC_UNAUTHORIZED);
+    response.getWriter().write("OAuth2 Login Failed: " + exception.getMessage());
   }
 }
