1e0ee2194c33f075b62cdeedbfe9c9708c3095cb
SGraceGrace <143794400+SGraceGrace@users.noreply.github.com>
2025-08-05 21:54:44 +0530

    header creation

--- foodieHub-FE/src/app/app.component.ts
+++ foodieHub-FE/src/app/app.component.ts
@@ -1,12 +1,16 @@
+import { CommonModule } from '@angular/common';
 import { Component, OnInit } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { HeaderComponent } from './header/header.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, CommonModule, HeaderComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
   title = 'foodieHub-FE';
+
+  role = 'guest';
 }
--- foodieHub-FE/src/app/header/header.component.ts
+++ foodieHub-FE/src/app/header/header.component.ts
@@ -1,8 +1,13 @@
 import { Component } from '@angular/core';
+import {MatButtonModule} from '@angular/material/button';
+import {MatToolbarModule} from '@angular/material/toolbar';
+import {MatIconModule} from '@angular/material/icon';
+import {MatMenuModule} from '@angular/material/menu';
 
 @Component({
   selector: 'app-header',
-  imports: [],
+  standalone: true,
+  imports: [MatToolbarModule, MatButtonModule, MatIconModule, MatMenuModule],
   templateUrl: './header.component.html',
   styleUrl: './header.component.scss'
 })
