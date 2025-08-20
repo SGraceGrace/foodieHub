be743f109c91f50ca10ad1d6a3abe3cad9b44339
SGraceGrace <143794400+SGraceGrace@users.noreply.github.com>
2025-08-20 21:41:41 +0530

    login page form craetion

--- foodieHub-FE/src/app/login/login.component.ts
+++ foodieHub-FE/src/app/login/login.component.ts
@@ -2,23 +2,44 @@ import { ChangeDetectionStrategy, Component, OnInit } from '@angular/core';
 import { CloudinaryImage } from '@cloudinary/url-gen/index';
 import { CloudinaryService } from '../shared/cloudinary.service';
 import { CommonModule } from '@angular/common';
-import {MatCardModule} from '@angular/material/card';
-import {MatChipsModule} from '@angular/material/chips';
-import {MatButtonModule} from '@angular/material/button';
+import { MatCardModule } from '@angular/material/card';
+import { MatChipsModule } from '@angular/material/chips';
+import { MatButtonModule } from '@angular/material/button';
+import {
+  FormBuilder,
+  FormGroup,
+  ReactiveFormsModule,
+  Validators,
+} from '@angular/forms';
 
 @Component({
   selector: 'app-login',
   standalone: true,
-  imports: [CommonModule, MatCardModule, MatChipsModule, MatButtonModule],
+  imports: [
+    CommonModule,
+    MatCardModule,
+    MatChipsModule,
+    MatButtonModule,
+    ReactiveFormsModule,
+  ],
   templateUrl: './login.component.html',
   styleUrl: './login.component.scss',
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class LoginComponent implements OnInit{
+export class LoginComponent implements OnInit {
   loginBigScreenUrl!: string;
   loginSmallScreenUrl!: string;
+  loginForm!: FormGroup;
 
-  constructor(private cloudinaryService: CloudinaryService) {};
+  constructor(
+    private cloudinaryService: CloudinaryService,
+    private fb: FormBuilder
+  ) {
+    this.loginForm = this.fb.group({
+      uname: ['', Validators.required],
+      pwd: ['', [Validators.required, Validators.minLength(8), Validators.maxLength(15), Validators.pattern('^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[!@#$%^&*])[A-Za-z0-9!@#$%^&*]{8,15}$')]],
+    });
+  }
 
   ngOnInit(): void {
     const cld = this.cloudinaryService.getInstance();
@@ -26,5 +47,10 @@ export class LoginComponent implements OnInit{
     this.loginSmallScreenUrl = cld.image('docs/loginbackgroundsmall').toURL();
   }
 
-
+  onSubmit() {
+    if (this.loginForm.valid) {
+      const { uname, pwd } = this.loginForm.value;
+      console.log('Form Submitted', { uname, pwd });
+    }
+  }
 }
