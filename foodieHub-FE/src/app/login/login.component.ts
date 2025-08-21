f8fc2ca366b2bec5a2b8e56e2d1930d1fb623145
SGraceGrace <143794400+SGraceGrace@users.noreply.github.com>
2025-08-21 20:33:23 +0530

    Password rules implementation

--- foodieHub-FE/src/app/login/login.component.ts
+++ foodieHub-FE/src/app/login/login.component.ts
@@ -30,6 +30,7 @@ export class LoginComponent implements OnInit {
   loginBigScreenUrl!: string;
   loginSmallScreenUrl!: string;
   loginForm!: FormGroup;
+  showRules: boolean = false;
 
   constructor(
     private cloudinaryService: CloudinaryService,
@@ -37,7 +38,17 @@ export class LoginComponent implements OnInit {
   ) {
     this.loginForm = this.fb.group({
       uname: ['', Validators.required],
-      pwd: ['', [Validators.required, Validators.minLength(8), Validators.maxLength(15), Validators.pattern('^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[!@#$%^&*])[A-Za-z0-9!@#$%^&*]{8,15}$')]],
+      pwd: [
+        '',
+        [
+          Validators.required,
+          Validators.minLength(8),
+          Validators.maxLength(15),
+          Validators.pattern(
+            '^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[!@#$%^&*])[A-Za-z0-9!@#$%^&*]{8,15}$'
+          ),
+        ],
+      ],
     });
   }
 
@@ -51,6 +62,41 @@ export class LoginComponent implements OnInit {
     if (this.loginForm.valid) {
       const { uname, pwd } = this.loginForm.value;
       console.log('Form Submitted', { uname, pwd });
+      this.loginForm.reset();
     }
+  }
+
+  hideRules() {
+    const pwdControl = this.loginForm.get('pwd');
+    if (!pwdControl?.value) {
+      this.showRules = false;
+    }
+  }
+
+  get pwd() {
+    return this.loginForm.get('pwd');
+  }
+
+  get pwdValue(): string {
+    return this.loginForm.get('pwd')?.value || '';
+  }
+
+  get isMinLength(): boolean {
+    return this.pwdValue.length >= 8;
+  }
+  get isMaxLength(): boolean {
+    return this.pwdValue.length <= 15 && this.pwdValue.length > 0;
+  }
+  get hasUppercase(): boolean {
+    return /[A-Z]/.test(this.pwdValue);
+  }
+  get hasLowercase(): boolean {
+    return /[a-z]/.test(this.pwdValue);
+  }
+  get hasNumber(): boolean {
+    return /[0-9]/.test(this.pwdValue);
+  }
+  get hasSpecial(): boolean {
+    return /[!@#$%^&*]/.test(this.pwdValue);
   }
 }
